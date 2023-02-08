30fe3db5f88c31acefed738c1f75c25732e88e45
rb718 <ghisodye@hotmail.com>
2023-02-08 12:30:13 -0600

    create the movie card component

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import MovieCard from "./movieCard";
 
 export default function SearchMovies() {
   const [query, setQuery] = useState("");
@@ -37,21 +38,11 @@ export default function SearchMovies() {
         </button>
       </form>
       <div className="card-list">
-        {movies.filter(movie => movie.poster_path).map((movie) => (
-          <div className="card" key={movie.id}>
-            <img
-              className="card--image"
-              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-              alt={`${movie.title} + poster`}
-            />
-            <div className="card--content">
-              <h3 className="card--title">{movie.title}</h3>
-              <p><small>RELEASE DATE: {movie.release_date}</small></p>
-              <p><small>RATING: {movie.vote_average}</small></p>
-              <p className="card--desc">{movie.overview}</p>
-            </div>
-          </div>
-        ))}
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
       </div>
     </>
   );
